b828400cfb4389eb1aacf7cb79903967954238a7
turalbabayev <turalbabayev@outlook.com.tr>
2024-03-03 07:54:13 +0300

    Initial Commit

---
+++ city-landmarks-app/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  city-landmarks-app
+//
+//  Created by Tural Babayev on 3.03.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    
+
+
+}
+
